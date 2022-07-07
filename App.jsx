ffd1c735b3f7f64d6045eda2bb2ddedcae0978fe
kusumakrish <80766178+kusumakrish@users.noreply.github.com>
2022-07-07 16:08:55 +0530

    Create App.jsx

---
+++ App.jsx
@@ -0,0 +1,25 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./component/Home";
+import Contact from "./component/contact";
+import About from "./component/About";
+import Nav from "./component/Nav";
+import NotFound from "./component/NotFound";
+import CompB from "./component/compB";
+import CompA from "./component/compA";
+export default function App() {
+  return (
+    <div className="App">
+      {/* <BrowserRouter>
+        <Nav />
+        <Routes>
+          <Route path="/Home" element={<Home />} />
+          <Route path="/Contact" element={<Contact />} />
+          <Route path="/About" element={<About />} />
+          <Route path="/*" element={<NotFound />} />
+        </Routes>
+      </BrowserRouter> */}
+      <CompA />
+      <CompB />
+    </div>
+  );
+}
